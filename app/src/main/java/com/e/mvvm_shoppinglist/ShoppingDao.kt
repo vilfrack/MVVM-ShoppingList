8086522146e7960ff38c018126d0e1b725b45446
Juan villarroel <vilfrack21@gmail.com>
2020-08-14 18:47:27 -0300

    Se agrega entyties

---
+++ app/src/main/java/com/e/mvvm_shoppinglist/ShoppingDao.kt
@@ -0,0 +1,17 @@
+package com.e.mvvm_shoppinglist
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface ShoppingDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(item: ShoppingItem)
+
+    @Delete
+    suspend fun delete(item: ShoppingItem)
+
+    @Query("SELECT * FROM shopping_item")
+    fun getAllShoppingItems(): LiveData<List<ShoppingItem>>
+}
